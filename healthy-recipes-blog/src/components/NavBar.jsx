9764b69dfdcd90f0737f40d3572f28480c56c3e4
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2025-07-16 16:05:34 +0300

    created the nav bar

--- healthy-recipes-blog/src/components/NavBar.jsx
+++ healthy-recipes-blog/src/components/NavBar.jsx
@@ -3,32 +3,62 @@ import Logo from "./Logo";
 
 export default function NavBar() {
   return (
-    <div className="pr-10 pl-1 w-full bg-yellow-500 flex items-center justify-between">
+    <div className="pr-10 pl-1 w-full bg-yellow-400 flex items-center justify-between">
       <NavLink to="home">
         <Logo />
       </NavLink>
       <nav>
         <ul className="flex items-center justify-evenly gap-10">
-          <li>Hello User</li>
+          <li className="text-xl italic text-green-950 font-semibold hover:underline transition-all duration-1000 cursor-pointer hover:text-black">
+            Hello User
+          </li>
           <li>
-            <NavLink className="text-lg" to="recipes">
+            <NavLink
+              className="text-xl italic text-green-950 font-semibold hover:underline transition-all duration-1000 cursor-pointer hover:text-black"
+              to="recipes"
+            >
               Recipes
             </NavLink>
           </li>
           <li>
-            <NavLink to="register">Register</NavLink>
+            <NavLink
+              className="text-xl italic text-green-950 font-semibold hover:underline transition-all duration-1000 cursor-pointer hover:text-black"
+              to="register"
+            >
+              Register
+            </NavLink>
           </li>
           <li>
-            <NavLink to="login">Login</NavLink>
+            <NavLink
+              className="text-xl italic text-green-950 font-semibold hover:underline transition-all duration-1000 cursor-pointer hover:text-black"
+              to="login"
+            >
+              Login
+            </NavLink>
           </li>
           <li>
-            <NavLink to="add-recipe">Add Recipe</NavLink>
+            <NavLink
+              className="text-xl italic text-green-950 font-semibold hover:underline transition-all duration-1000 cursor-pointer hover:text-black"
+              to="add-recipe"
+            >
+              Add Recipe
+            </NavLink>
           </li>
           <li>
-            <NavLink to="about">About</NavLink>
+            <NavLink
+              className="text-xl italic text-green-950 font-semibold hover:underline transition-all duration-1000 cursor-pointer hover:text-black"
+              to="about"
+            >
+              About
+            </NavLink>
           </li>
           <li>
-            <NavLink to="logout">Logout</NavLink>
+            <NavLink
+              className="text-xl italic text-green-950 font-semibold hover:underline transition-all duration-500 cursor-pointer hover:text-black"
+              to="logout"
+            >
+              Logout
+            </NavLink>
           </li>
         </ul>
       </nav>
