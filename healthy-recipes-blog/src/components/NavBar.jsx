2543787ff8ace944c04912c43391549d4635b641
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2025-07-23 16:28:06 +0300

    nav bar shows different routes depending on whether user is authenticated

--- healthy-recipes-blog/src/components/NavBar.jsx
+++ healthy-recipes-blog/src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
+import { useAuth } from "../context/AuthenticationContext";
 
 export default function NavBar() {
+  const { userEmail, isAuthenticated } = useAuth();
   return (
     <div className="pr-10 pl-1 w-full bg-green-300 flex items-center justify-between">
       <NavLink to="home">
@@ -10,7 +12,9 @@ export default function NavBar() {
       <nav>
         <ul className="flex items-center justify-evenly gap-10">
           <li className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600">
-            Hello User
+            {isAuthenticated && userEmail
+              ? `Welcome, ${userEmail}`
+              : "Guest User"}
           </li>
           <li>
             <NavLink
@@ -20,38 +24,46 @@ export default function NavBar() {
               Recipes
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600"
-              to="register"
-            >
-              Register
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600"
-              to="login"
-            >
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600"
-              to="add-recipe"
-            >
-              Add Recipe
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600"
-              to="logout"
-            >
-              Logout
-            </NavLink>
-          </li>
+          {!isAuthenticated && (
+            <>
+              <li>
+                <NavLink
+                  className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600"
+                  to="register"
+                >
+                  Register
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600"
+                  to="login"
+                >
+                  Login
+                </NavLink>
+              </li>
+            </>
+          )}
+          {isAuthenticated && (
+            <>
+              <li>
+                <NavLink
+                  className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600"
+                  to="add-recipe"
+                >
+                  Add Recipe
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="text-2xl italic text-green-950 font-semibold hover:underline transition-all duration-300 cursor-pointer hover:text-yellow-600"
+                  to="logout"
+                >
+                  Logout
+                </NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </div>
